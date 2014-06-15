da081e73e5803d87d2836ce5e57527f091b6b501
wubaiqing <wubaiqing@vip.qq.com>
2014-06-15 23:24:06 +0800

    修改logo以及关闭按钮

--- frontend/app/views/layouts/master.blade.php
+++ frontend/app/views/layouts/master.blade.php
@@ -4,10 +4,10 @@
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 	<title>满意投—打造国内最大的电商资源广告联盟</title>
-	<link rel="stylesheet" type="text/css" href="{{ asset('static/css/base-min.css') }}">
+	<link rel="stylesheet" type="text/css" href="{{ asset('static/css/base-min.css?v=1.0.1') }}">
 	<link rel="stylesheet" type="text/css" href="{{ asset('static/css/index.css') }}">
 	<link rel="stylesheet" type="text/css" href="{{ asset('static/css/page.css') }}">
-	<link rel="stylesheet" type="text/css" href="{{ asset('static/css/colorbox.css') }}">
+	<link rel="stylesheet" type="text/css" href="{{ asset('static/css/colorbox.css?v=1.0.1') }}">
 	<script src="http://libs.baidu.com/jquery/1.10.2/jquery.min.js"></script>
 	<script src="{{ asset('static/js/jquery.colorbox.js') }}"></script>
 	<script src="{{ asset('static/js/jquery.lazyload.js') }}"></script>
