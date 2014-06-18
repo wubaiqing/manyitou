5c5aff685758e70e3d7c9cb5d80f686d046f26c1
wubaiqing <wubaiqing@vip.qq.com>
2014-06-19 01:03:27 +0800

    关于我们链接

--- frontend/app/views/layouts/footer.blade.php
+++ frontend/app/views/layouts/footer.blade.php
@@ -5,7 +5,7 @@
 			Copyright 2014，版权所有
 		</div>
 		<div class="foot_link">
-			<a href="javascript:void(0);" target="_blank">关于我们</a>
+			<a href="http://www.manyitou.com/billboard/detail/1.html" target="_blank">关于我们</a>
 			<a href="javascript:void(0);" target="_blank">联系我们</a>
 		</div>
 	</div>
