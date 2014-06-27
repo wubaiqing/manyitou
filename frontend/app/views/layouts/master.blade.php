04f8d19a1604a8d13f6db0977ce40b0bb91ad9c0
wubaiqing <wubaiqing@vip.qq.com>
2014-06-27 21:55:33 +0800

    修改满意头图片

--- frontend/app/views/layouts/master.blade.php
+++ frontend/app/views/layouts/master.blade.php
@@ -4,6 +4,7 @@
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 	<title>满意投—打造国内最大的电商资源广告联盟</title>
+	<link rel="shortcut icon" href="http://myt.oss-cn-hangzhou.aliyuncs.com/static/favicon/%E5%B0%8F%E5%9B%BE%E6%A0%87.ico" type="image/x-icon">
 	<link rel="stylesheet" type="text/css" href="{{ asset('static/css/base-min.css?v=1.0.5') }}">
 	<link rel="stylesheet" type="text/css" href="{{ asset('static/css/index.css?v=1.0.8') }}">
 	<link rel="stylesheet" type="text/css" href="{{ asset('static/css/page.css?v=1.0.5') }}">
