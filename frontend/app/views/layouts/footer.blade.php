d546ebea1aac2a3ea5d64090392933c9010c357e
wubaiqing <wubaiqing@vip.qq.com>
2014-07-01 18:05:52 +0800

    添加版权号

--- frontend/app/views/layouts/footer.blade.php
+++ frontend/app/views/layouts/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="foot_content">
         <div class="foot_info">
             <a href="/">满意投</a>
-            Copyright 2014，版权所有
+            Copyright 2014，版权所有 京ICP备14028259号
         </div>
         <div class="foot_link">
             <a href="http://www.manyitou.com/billboard/detail/1.html" target="_blank">关于我们</a>
