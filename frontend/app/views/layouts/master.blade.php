4498761a03acc7344cb5baef6e2bc9429a31a213
wubaiqing <wubaiqing@vip.qq.com>
2014-07-02 22:34:40 +0800

    update css version

--- frontend/app/views/layouts/master.blade.php
+++ frontend/app/views/layouts/master.blade.php
@@ -6,7 +6,7 @@
     <title>满意投—打造国内最大的电商资源广告联盟</title>
     <link rel="shortcut icon" href="http://myt.oss-cn-hangzhou.aliyuncs.com/static/favicon/%E5%B0%8F%E5%9B%BE%E6%A0%87.ico" type="image/x-icon">
     <link rel="stylesheet" type="text/css" href="{{ asset('static/css/base-min.css?v=1.0.5') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('static/css/index.css?v=1.0.8') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('static/css/index.css?v=1.0.9') }}">
     <link rel="stylesheet" type="text/css" href="{{ asset('static/css/page.css?v=1.0.5') }}">
     <link rel="stylesheet" type="text/css" href="{{ asset('static/css/colorbox.css?v=1.0.5') }}">
     <script src="http://libs.baidu.com/jquery/1.10.2/jquery.min.js"></script>
