ae52358f8fb2501be096ebc31ef3d8f8f854ec95
wubaiqing <wubaiqing@vip.qq.com>
2015-10-28 17:23:17 +0800

    详情页改版

--- frontend/app/views/site/detail.blade.php
+++ frontend/app/views/site/detail.blade.php
@@ -12,39 +12,68 @@
                         <span class="sep"> &gt; </span>
                         <span><a href="{{ URL::to('lists.html')}}">活动中心</a></span>
                         <span class="sep"> &gt; </span>
-                        <span><a href="">{{ $detail['platform_name'] }}</a></span>
+                        <span><a href="#">{{ $detail['platform_name'] }}</a></span>
                     </div>
                     <div class="list">
                         <div class="list-t">
                             <div class="siteinfo">网站信息</div>
-                            <div class="sitegobuy">活动价格</div>
+                            <div class="sitegobuy">简介</div>
                         </div>
                         <div class="list-c clearfix">
                             <div class="list-left">
                                 <a href="" class="img" target="_blank">
                                     <img src="{{ $detail['logo'] }}">
                                 </a>
+
                                 <div class="site-info">
                                     <div class="siteinfo-top">
-                                        <h3>{{ $detail['platform_name'] }}<i class="wz"></i></h3>
-                                        <span class="wz-info">{{ $detail['playform_desc'] }}</span>
-                                    <span class="link">
-                                        <a href="{{ $detail['playform_url'] }}" target="_blank">{{ $detail['playform_url'] }}</a>
-                                    </span>
-                                    </div>
-                                    <div class="siteinfo-bottom">
-                                        <h3>活动要求<i class="uz"></i></h3>
-                                    <span class="uz-intro">
-                                        {{ $detail['rules'] }}
+                                        <span class="wz-info">
+                                            <span>资源名称</span>
+                                            {{ $detail['platform_name'] }}
+                                        </span>
 
-                                    </span>
-                                        <span class="link"><a href="" target="_blank"></a></span>
+                                        <span class="wz-info">
+                                            <span>网址</span>
+                                            <a href="{{ $detail['playform_url'] }}" target="_blank">{{ $detail['playform_url'] }}</a>
+                                        </span>
+
+                                        <span class="wz-info">
+                                            <span>类型</span>
+                                            {{ Goods::$types[$detail['type']] }}
+                                        </span>
+
+                                        <span class="wz-info">
+                                            <span>适合推广平台</span>
+                                            {{ Goods::$generalizePlatforms[$detail['generalize_platform']] }}
+                                        </span>
+
+                                        <span class="wz-info">
+                                            <span>适合推广类目</span>
+                                            {{ Goods::$generalizeTypes[$detail['generalize_type']] }}
+                                        </span>
+
+                                        <span class="wz-info">
+                                            <span>适合推广类目</span>
+                                            {{ Goods::$generalizeTypes[$detail['generalize_type']] }}
+                                        </span>
+
+                                        <span class="wz-info">
+                                            <span>费用</span>
+                                            {{ Goods::$cooperationTypes[$detail['cooperation_type']] }}
+                                        </span>
+
+                                        <span class="wz-info">
+                                            <span>联系方式</span>
+                                            <a href="#">查看</a>
+
+                                        </span>
+
                                     </div>
                                 </div>
                             </div>
                             <div class="list-right">
                                 <div class="right-content">
-                                    {{ $detail['price_text'] }}
+                                    {{ $detail['playform_desc'] }}
                                 </div>
                             </div>
                             <div style="clear:both;"></div>
@@ -54,6 +83,7 @@
             </div>
         </div>
 
+        @if ($detail['claim_image'])
         <div id="about" class="clearfix">
             <div class="content-navbar" id="J-content-navbar">
                 <ul>
@@ -66,6 +96,20 @@
                 <a href="{{ $detail['claim_image'] }}" class="imageBox">
                     <img src="{{ $detail['claim_image'] }}" />
                 </a>
+            </div>
+        </div>
+        @endif
+
+        <div id="about" class="clearfix">
+            <div class="content-navbar" id="J-content-navbar">
+                <ul>
+                    <li div-id="deal-record" class="content-navbar__item--current">
+                        <a href="javascript:;" class="tab-item">推荐理由</a>
+                    </li>
+                </ul>
+            </div>
+            <div id="deal-record" style="display:block;">
+                {{ $detail['recommended_desc'] }}
             </div>
         </div>
     </div>
