60c565d326055244f76d0a61b8bac784f0d6d543
wubaiqing <wubaiqing@vip.qq.com>
2015-10-28 18:34:27 +0800

    首页布局

--- frontend/app/views/site/banner.blade.php
+++ frontend/app/views/site/banner.blade.php
@@ -16,7 +16,54 @@
 		<script type="text/javascript">
 		</script>
 	</div>
+
+	<style>
+		.head-top-login {
+			height: 104px;
+		}
+		.head-top-login a{
+			color: white;
+			display: inline-block;
+			-webkit-border-radius: 2px;
+			-moz-border-radius: 2px;
+			-ms-border-radius: 2px;
+			-o-border-radius: 2px;
+			border-radius: 2px;
+			width: 130px;
+			margin-right: 20px;
+			height: 32px;
+			line-height: 32px;
+			float: left;
+			text-align: center;
+			cursor: pointer;
+			text-decoration: none;
+		}
+
+		.head-top-login span {
+		}
+
+		.head-top-login .login-1 {
+			background-color: #f04848;
+			margin-top: 30px;
+		}
+
+		.head-top-login .login-2 {
+			margin-top: 30px;
+			background-color: #2e76a8;
+
+		}
+		.login {
+			float: right;
+			width: 300px;
+			overflow: hidden;
+		}
+	</style>
+
 	<div class="login" id="index_login">
+		<div class="head-top-login">
+			<a href="#" target="_blank" class="login-1">登录</a>
+			<a href="#" target="_blank" class="login-2">注册</a>
+		</div>
 		<ul class="tabs">
 			<li class="selected">
 				<a href="{{ URL::to('billboard/list.html') }}">网站公告</a>
