f23057b0535a7d741d80ddd86a2f9c8508260445
wubaiqing <wubaiqing@vip.qq.com>
2015-10-29 21:43:20 +0800

    资源中心

--- frontend/app/views/site/category.blade.php
+++ frontend/app/views/site/category.blade.php
@@ -42,7 +42,7 @@
         <div class="item-dt">推广类型:</div>
         <div class="item-dl">
             @foreach( Goods::$types as $key => $item )
-                <a class="{{ $type == $key ? 'cur' : '' }}" href="{{ URL::to("?type=$key&generalize_type=$generalizeType&generalize_platform=$generalizePlatform&cooperation_type=$cooperationType" ) }}">{{ $item }}</a>
+                <a class="{{ $type == $key ? 'cur' : '' }}" href="{{ URL::to(URL::current() . "?type=$key&generalize_type=$generalizeType&generalize_platform=$generalizePlatform&cooperation_type=$cooperationType" ) }}">{{ $item }}</a>
             @endforeach
         </div>
 
@@ -52,7 +52,7 @@
         <div class="item-dt">推广类目:</div>
         <div class="item-dl">
             @foreach( Goods::$generalizeTypes as $key => $item )
-                <a class="{{ $generalizeType == $key ? 'cur' : '' }}" href="{{ URL::to("?type=$type&generalize_type=$key&generalize_platform=$generalizePlatform&cooperation_type=$cooperationType") }}">{{ $item }}</a>
+                <a class="{{ $generalizeType == $key ? 'cur' : '' }}" href="{{ URL::to(URL::current() . "?type=$type&generalize_type=$key&generalize_platform=$generalizePlatform&cooperation_type=$cooperationType") }}">{{ $item }}</a>
             @endforeach
         </div>
 
@@ -61,7 +61,7 @@
         <div class="item-dt">推广平台:</div>
         <div class="item-dl">
             @foreach( Goods::$generalizePlatforms as $key => $item )
-                <a class="{{ $generalizePlatform == $key ? 'cur' : '' }}" href="{{ URL::to("?type=$type&generalize_type=$generalizeType&generalize_platform=$key&cooperation_type=$cooperationType") }}">{{ $item }}</a>
+                <a class="{{ $generalizePlatform == $key ? 'cur' : '' }}" href="{{ URL::to(URL::current() . "?type=$type&generalize_type=$generalizeType&generalize_platform=$key&cooperation_type=$cooperationType") }}">{{ $item }}</a>
             @endforeach
         </div>
 
@@ -70,7 +70,7 @@
         <div class="item-dt">合作方式:</div>
         <div class="item-dl">
             @foreach( Goods::$priceTypes as $key => $item )
-                <a class="{{ $cooperationType == $key ? 'cur' : '' }}" href="{{ URL::to("?type=$type&generalize_type=$generalizeType&generalize_platform=$generalizePlatform&cooperation_type=$key") }}">{{ $item }}</a>
+                <a class="{{ $cooperationType == $key ? 'cur' : '' }}" href="{{ URL::to(URL::current() . "?type=$type&generalize_type=$generalizeType&generalize_platform=$generalizePlatform&cooperation_type=$key") }}">{{ $item }}</a>
             @endforeach
         </div>
 
