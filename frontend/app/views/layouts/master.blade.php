57d6f1ea35a907dfcf721fd0d4ea82882255bd50
wubaiqing <wubaiqing@vip.qq.com>
2015-11-02 15:18:41 +0800

    webkit

--- frontend/app/views/layouts/master.blade.php
+++ frontend/app/views/layouts/master.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <title>满意投—打造国内最大的电商资源广告联盟</title>
-    <meta name="renderer" content="webkit|ie-comp|ie-stand">
+    <meta name="renderer" content="webkit">
 	<meta name="keywords" content="满意投,manyitou,淘外广告,电商广告,店铺推广">
 	<meta name="description" content="满意投广告联盟是一家为专注电子商务行业推广的广告联盟。目前是做国内最专业的电商营销推广联盟，我们具有一套专业、精准的广告投放体系，在流量监测，资源整合方面有着多年的实战经验，宗旨是让客户流量价值最大化。">
     <link rel="shortcut icon" href="http://myt.oss-cn-hangzhou.aliyuncs.com/static/favicon/%E5%B0%8F%E5%9B%BE%E6%A0%87.ico" type="image/x-icon">
